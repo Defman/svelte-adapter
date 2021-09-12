f31265fdbeff842d27cf980b073df0cc3e57e321
Jacob Emil Ulvedal Rosborg <jacob@rosborg.dk>
2021-09-12 23:24:11 +0200

    ...

--- files/entry.js
+++ files/entry.js
@@ -1,6 +1,14 @@
 import { init, render } from "../output/server/app.js";
 import { getAssetFromKV, NotFoundError } from "@cloudflare/kv-asset-handler";
 
+const baseHeaders = {
+	"X-Content-Type-Options": "nosniff",
+	"X-Frame-Options": "DENY",
+	"Content-Security-Policy": "default-src 'self'",
+	"X-XSS-Protection": "1; mode=block",
+	"Cache-Control": "max-age=600",
+};
+
 const staticCacheControl = {
 	browserTTL: 2 * 60 * 60 * 24,
 	edgeTTL: 2 * 60 * 60 * 24,
@@ -10,17 +18,8 @@ const staticCacheControl = {
 init();
 
 addEventListener("fetch", (event) => {
-	event.respondWith(headers(handle(event)));
+	event.respondWith(handle(event));
 });
-
-function headers(response) {
-	response.headers.append("X-Content-Type-Options", "nosniff");
-	response.headers.append("X-Frame-Options", "DENY");
-	response.headers.append("Content-Security-Policy", "default-src 'self'");
-	response.headers.append("X-XSS-Protection", "1; mode=block");
-	response.headers.set("Cache-Control", "max-age=600")
-	return response;
-}
 
 async function handle(event) {
 	// try static files first
@@ -28,7 +27,12 @@ async function handle(event) {
 		try {
 			// TODO rather than attempting to get an asset,
 			// use the asset manifest to see if it exists
-			return await getAssetFromKV(event, { cacheControl: staticCacheControl });
+			const response = await getAssetFromKV(event, { cacheControl: staticCacheControl });
+			const newResponse = new Response(response.body, { 
+				...response, 
+				headers: makeHeaders(baseHeaders, new Headers(response.headers)) 
+			});
+			return newResponse;
 		} catch (e) {
 			if (!(e instanceof NotFoundError)) {
 				return new Response("Error loading static asset:" + (e.message || e.toString()), {
@@ -55,16 +59,19 @@ async function handle(event) {
 		if (rendered) {
 			return new Response(rendered.body, {
 				status: rendered.status,
-				headers: makeHeaders(rendered.headers)
+				headers: makeHeaders({ ...baseHeaders, ...rendered.headers })
 			});
 		}
 	} catch (e) {
-		return new Response("Error rendering route:" + (e.message || e.toString()), { status: 500 });
+		return new Response("Error rendering route:" + (e.message || e.toString()), {
+			status: 500, headers: makeHeaders({ ...baseHeaders, "Cache-Control": "no-cache" })
+		});
 	}
 
 	return new Response({
 		status: 404,
-		statusText: "Not Found"
+		statusText: "Not Found",
+		headers: makeHeaders({ ...baseHeaders, "Cache-Control": "no-cache" })
 	});
 }
 
@@ -77,8 +84,7 @@ async function read(request) {
  * @param {Record<string, string | string[]>} headers
  * @returns {Request}
  */
-function makeHeaders(headers) {
-	const result = new Headers();
+function makeHeaders(headers, result = new Header()) {
 	for (const header in headers) {
 		const value = headers[header];
 		if (typeof value === "string") {
